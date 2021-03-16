ff054debd3671c58ec8b45c908d1866db6d1de66
tjayrush <jrush@quickblocks.io>
2021-03-16 11:48:09 -0400

    Fixing

--- update_site.sh
+++ update_site.sh
@@ -3,9 +3,7 @@
 cd pouch
 # touch *.cpp
 make
-bin/pouch >tmp.js
-cat tmp.js >../src/grants-data.js
-rm -f tmp.js
+bin/pouch --freshen
 cd -
 yarn build
 yes | cp -pr build/* /home/jrush/Websites/tokenomics.io/
