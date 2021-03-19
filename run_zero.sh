d767a277aa483be6e6e48544b3c05deac8396bbe
tjayrush <jrush@greathill.com>
2021-03-19 07:07:07 +0000

    Updating run_zero.sh

--- run_zero.sh
+++ run_zero.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-#chifra export --appearances $1 | cut -f2,3 >pouch/data/apps/$1.txt
-chifra export --logs --articulate --cache_txs --cache_traces --fmt json $1 >pouch/data/$1.json
-#chifra export --logs --articulate --cache_txs --cache_traces --fmt csv $1 >pouch/data/$1.csv
+export DICT_MODE=true
+
+chifra export --appearances $1 | cut -f2,3 >pouch/data/apps/$1.txt
+
+chifra export \
+    --logs --articulate --cache_txs --cache_traces \
+    --fmt json $1 >pouch/data/$1.json
+
+chifra export \
+    --logs --articulate --cache_txs --cache_traces \
+    --fmt csv $1 >pouch/data/$1.csv
+
 sleep .3
