9bdd544f0cb595b7d3e53d4c73822149f935cecd
tjayrush <jrush@quickblocks.io>
2021-03-30 07:11:43 -0400

    Updating data

--- pouch/options.cpp
+++ pouch/options.cpp
@@ -127,7 +127,6 @@ void COptions::Init(void) {
 
 //---------------------------------------------------------------------------------------------------
 COptions::COptions(void) {
-    setSorts(GETRUNTIME_CLASS(CBlock), GETRUNTIME_CLASS(CTransaction), GETRUNTIME_CLASS(CReceipt));
     tsMemMap = nullptr;
     tsCnt = 0;
     Init();
