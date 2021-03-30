2a859bfb9d93f26a6037641a17a63b411ab55c13
tjayrush <jrush@quickblocks.io>
2021-03-30 08:32:38 -0400

    Updating data for nerds

--- src/DataForNerds.jsx
+++ src/DataForNerds.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
-// import { DownloadIcon } from './Utils'
+import {Typography} from 'antd';
 
+const {Link} = Typography;
 
 export const DataForNerds = () => {
   var record = { "tx_cnt": "1" };
   return (
     <div style={{textAlign: 'left'}}>
-      <h3>Coming soon...</h3>
+      <center>
+        <h3>Coming soon...</h3>
+        <a rel='noreferrer' target='_blank' href='https://github.com/TrueBlocks/tokenomics.io/issues'>
+          <Link>Have a suggestion?</Link>
+        </a>
+      </center>
       <ul>
+        <li>Percent of total unique donors</li>
         <li>Uniq recipients by date</li>
         <li>Uniq donors by count</li>
         <li>Uniq recipients by count</li>
